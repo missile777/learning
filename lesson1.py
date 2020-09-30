541caef5d7fdc3d146e0532dadb29c1ad1cc67c8
Phillip Flores <phillipf@uci.edu>
2020-09-30 15:16:56 -0700

    Added 3 and functions test cases: [1] get evens [2] get odds [3] get thirds

--- lesson1.py
+++ lesson1.py
@@ -9,6 +9,10 @@ def get_odds(n: list):
     return [i for i in n if i % 2 == 1]
 
 
+def get_thirds(n: list):
+    return [i for i in n if i % 3 == 0]
+
+
 class TestEvenMethod(unittest.TestCase):
     def test_get_evens(self):
         nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
@@ -18,9 +22,8 @@ class TestEvenMethod(unittest.TestCase):
         nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
         self.assertListEqual(get_odds(nums), [1, 3, 5, 7, 9, 11, 13, 15, 17])
 
-
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-print(get_evens(nums))
-print(get_odds(nums))
+    def test_get_thirds(self):
+        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+        self.assertListEqual(get_thirds(nums), [3, 6, 9, 12, 15])
 
 unittest.main()
