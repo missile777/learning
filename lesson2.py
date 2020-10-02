ca9c8f8766d170255dbd6d9fa3d3b33db41c5507
Phillip Flores <phillipf@uci.edu>
2020-10-01 19:34:22 -0700

    Added lesson2.py, for list methods and some test cases

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+def reverse(n: list):
+    # do this function
+    return n
+
+
+class TestListMethod(unittest.TestCase):
+    def test_get_reverse1(self):
+        nums = [10, 20, 40, 80]
+        self.assertListEqual(reverse(nums), [80, 40, 20, 10])
+
+    def test_get_reverse2(self):
+        nums = [10, 20, 40, 40, 80]
+        self.assertListEqual(reverse(nums), [80, 40, 40, 20, 10])
+
+
+if __name__ == '__main__':
+    unittest.main()
